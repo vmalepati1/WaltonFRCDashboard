d25e17b5525e43b88a0cf6003c38917d15d2d517
vmalepati1 <malepativikas@gmail.com>
2019-05-24 00:02:05 -0400

    Added subtable prefix to network table keys and made minor UI changes in progress circles. -VM
    Added subtable prefix to network table keys and made minor UI changes in progress circles. -VM

--- DeepSpace/src/ui.js
+++ DeepSpace/src/ui.js
@@ -2,81 +2,81 @@ let ui = {
     timer: document.getElementById('w-node-14012e7d8a0d-6cf0ea0e'),
     robotState: document.getElementById('w-node-6ca20209b32e-6cf0ea0e'),
 	configurationBox: [
-		[document.getElementById('Constants-KV'), 'Constants/KV'],
-		[document.getElementById('Constants-KAcc'), 'Constants/KAcc'],
-		[document.getElementById('Constants-KK'), 'Constants/KK'],
-		[document.getElementById('Constants-KS'), 'Constants/KS'],
-		[document.getElementById('Constants-KAngle'), 'Constants/KAngle'],
-		[document.getElementById('Constants-Max-Velocity'), 'Constants/Max Velocity'],
-		[document.getElementById('Constants-Max-Acceleration'), 'Constants/Max Acceleration'],
-		[document.getElementById('Constants-KL'), 'Constants/KL'],
-		[document.getElementById('Motion-Front-Rocket-X'), 'Motion/Front Rocket X'],
-		[document.getElementById('Transform-Select'), 'Driveteam/Transform Select'],
-		[document.getElementById('Motion-Front-Rocket-Y'), 'Motion/Front Rocket Y'],
-		[document.getElementById('Motion-Front-Rocket-Angle'), 'Motion/Front Rocket Angle'],
-		[document.getElementById('Motion-BackUp-X'), 'Motion/BackUp X'],
-		[document.getElementById('Motion-BackUp-Y'), 'Motion/BackUp Y'],
-		[document.getElementById('Motion-BackUp-Angle'), 'Motion/BackUp Angle'],
-		[document.getElementById('Motion-Hatch-PickUp-X'), 'Motion/Hatch PickUp X'],
-		[document.getElementById('Motion-Hatch-PickUp-Y'), 'Motion/Hatch PickUp Y'],
-		[document.getElementById('Motion-Hatch-PickUp-Angle'), 'Motion/Hatch PickUp Angle'],
+		[document.getElementById('Constants-KV'), '/WaltonDashboard/Constants/KV'],
+		[document.getElementById('Constants-KAcc'), '/WaltonDashboard/Constants/KAcc'],
+		[document.getElementById('Constants-KK'), '/WaltonDashboard/Constants/KK'],
+		[document.getElementById('Constants-KS'), '/WaltonDashboard/Constants/KS'],
+		[document.getElementById('Constants-KAngle'), '/WaltonDashboard/Constants/KAngle'],
+		[document.getElementById('Constants-Max-Velocity'), '/WaltonDashboard/Constants/Max Velocity'],
+		[document.getElementById('Constants-Max-Acceleration'), '/WaltonDashboard/Constants/Max Acceleration'],
+		[document.getElementById('Constants-KL'), '/WaltonDashboard/Constants/KL'],
+		[document.getElementById('Motion-Front-Rocket-X'), '/WaltonDashboard/Motion/Front Rocket X'],
+		[document.getElementById('Transform-Select'), '/WaltonDashboard/Driveteam/Transform Select'],
+		[document.getElementById('Motion-Front-Rocket-Y'), '/WaltonDashboard/Motion/Front Rocket Y'],
+		[document.getElementById('Motion-Front-Rocket-Angle'), '/WaltonDashboard/Motion/Front Rocket Angle'],
+		[document.getElementById('Motion-BackUp-X'), '/WaltonDashboard/Motion/BackUp X'],
+		[document.getElementById('Motion-BackUp-Y'), '/WaltonDashboard/Motion/BackUp Y'],
+		[document.getElementById('Motion-BackUp-Angle'), '/WaltonDashboard/Motion/BackUp Angle'],
+		[document.getElementById('Motion-Hatch-PickUp-X'), '/WaltonDashboard/Motion/Hatch PickUp X'],
+		[document.getElementById('Motion-Hatch-PickUp-Y'), '/WaltonDashboard/Motion/Hatch PickUp Y'],
+		[document.getElementById('Motion-Hatch-PickUp-Angle'), '/WaltonDashboard/Motion/Hatch PickUp Angle'],
 		[document.getElementById('Autonomous-Select'), 'Auton']
 	],
 	diagnosticsTiles: [
-		[document.getElementById('w-node-ece068a8fbe3-6cf0ea0e'), 'Motors/Elevator Height'],
-		[document.getElementById('w-node-6456cc18dbb4-6cf0ea0e'), 'Motors/Hatch Angle'],
-		[document.getElementById('w-node-795b9b2337a4-6cf0ea0e'), 'Motors/Intake Open'],
-		[document.getElementById('w-node-8e498667928f-6cf0ea0e'), 'Drivetrain/rightMotor'],
-		[document.getElementById('w-node-d58ddcae3769-6cf0ea0e'), 'Motors/Cargo Angle'],
-		[document.getElementById('w-node-33d94c8c1081-6cf0ea0e'), 'Motors/Lower Limit'],
-		[document.getElementById('w-node-d4c27eac3f93-6cf0ea0e'), 'Motors/Elevator Forwards Soft Limit'],
-		[document.getElementById('w-node-45d40d43c455-6cf0ea0e'), 'Motors/Elevator Reverse Soft Limit'],
-		[document.getElementById('w-node-633d385417e5-6cf0ea0e'), 'Motors/Claw Forwards Soft Limit'],
-		[document.getElementById('w-node-2b4d1c09f1c8-6cf0ea0e'), 'Motors/Claw Reverse Soft Limit'],
-		[document.getElementById('w-node-15b66f493392-6cf0ea0e'), 'Motors/Hatch Forwards Soft Limit'],
-		[document.getElementById('w-node-65026c0c8959-6cf0ea0e'), 'Motors/Hatch Reverse Soft Limit'],
-		[document.getElementById('w-node-0874236dabf0-6cf0ea0e'), 'Motors/State'],
-		[document.getElementById('w-node-230f5958393e-6cf0ea0e'), 'Motors/Elevator Mode'],
-		[document.getElementById('w-node-0827ba0ef127-6cf0ea0e'), 'Motors/Cargo Mode'],
-		[document.getElementById('w-node-fc40adc9d11f-6cf0ea0e'), 'Motors/Hatch Mode'],
-		[document.getElementById('w-node-3dae72b756e1-6cf0ea0e'), 'Motors/Elevator Power'],
-		[document.getElementById('w-node-6ab5de7d522d-6cf0ea0e'), 'Motors/Elevator Target'],
-		[document.getElementById('w-node-5c897ecfedd4-6cf0ea0e'), 'Motors/Hatch Power'],
-		[document.getElementById('w-node-2dd40fb21b0e-6cf0ea0e'), 'Motors/Hatch Target'],
-		[document.getElementById('w-node-2d309eb47a59-6cf0ea0e'), 'Motors/Cargo Power'],
-		[document.getElementById('w-node-4e1151804e61-6cf0ea0e'), 'Motors/Cargo Target'],
-		[document.getElementById('w-node-1a75b8348af0-6cf0ea0e'), 'Motors/Climber Power'],
-		[document.getElementById('w-node-e58751785c7a-6cf0ea0e'), 'Motors/Climber Mode'],
-		[document.getElementById('w-node-828d4271a53a-6cf0ea0e'), 'Drivetrain/Position'],
-		[document.getElementById('w-node-9971a8c5e074-6cf0ea0e'), 'Drivetrain/Encoder Right'],
-		[document.getElementById('w-node-04c10c4157ee-6cf0ea0e'), 'Drivetrain/Encoder Left'],
-		[document.getElementById('w-node-39646deb58ba-6cf0ea0e'), 'Drivetrain/leftSpeed'],
-		[document.getElementById('w-node-6a6121c16e8e-6cf0ea0e'), 'Drivetrain/rightSpeed'],
-		[document.getElementById('w-node-75d3c40bd623-6cf0ea0e'), 'Drivetrain/leftMotor']
+		[document.getElementById('w-node-ece068a8fbe3-6cf0ea0e'), '/WaltonDashboard/Motors/Elevator Height'],
+		[document.getElementById('w-node-6456cc18dbb4-6cf0ea0e'), '/WaltonDashboard/Motors/Hatch Angle'],
+		[document.getElementById('w-node-795b9b2337a4-6cf0ea0e'), '/WaltonDashboard/Motors/Intake Open'],
+		[document.getElementById('w-node-8e498667928f-6cf0ea0e'), '/WaltonDashboard/Drivetrain/rightMotor'],
+		[document.getElementById('w-node-d58ddcae3769-6cf0ea0e'), '/WaltonDashboard/Motors/Cargo Angle'],
+		[document.getElementById('w-node-33d94c8c1081-6cf0ea0e'), '/WaltonDashboard/Motors/Lower Limit'],
+		[document.getElementById('w-node-d4c27eac3f93-6cf0ea0e'), '/WaltonDashboard/Motors/Elevator Forwards Soft Limit'],
+		[document.getElementById('w-node-45d40d43c455-6cf0ea0e'), '/WaltonDashboard/Motors/Elevator Reverse Soft Limit'],
+		[document.getElementById('w-node-633d385417e5-6cf0ea0e'), '/WaltonDashboard/Motors/Claw Forwards Soft Limit'],
+		[document.getElementById('w-node-2b4d1c09f1c8-6cf0ea0e'), '/WaltonDashboard/Motors/Claw Reverse Soft Limit'],
+		[document.getElementById('w-node-15b66f493392-6cf0ea0e'), '/WaltonDashboard/Motors/Hatch Forwards Soft Limit'],
+		[document.getElementById('w-node-65026c0c8959-6cf0ea0e'), '/WaltonDashboard/Motors/Hatch Reverse Soft Limit'],
+		[document.getElementById('w-node-0874236dabf0-6cf0ea0e'), '/WaltonDashboard/Motors/State'],
+		[document.getElementById('w-node-230f5958393e-6cf0ea0e'), '/WaltonDashboard/Motors/Elevator Mode'],
+		[document.getElementById('w-node-0827ba0ef127-6cf0ea0e'), '/WaltonDashboard/Motors/Cargo Mode'],
+		[document.getElementById('w-node-fc40adc9d11f-6cf0ea0e'), '/WaltonDashboard/Motors/Hatch Mode'],
+		[document.getElementById('w-node-3dae72b756e1-6cf0ea0e'), '/WaltonDashboard/Motors/Elevator Power'],
+		[document.getElementById('w-node-6ab5de7d522d-6cf0ea0e'), '/WaltonDashboard/Motors/Elevator Target'],
+		[document.getElementById('w-node-5c897ecfedd4-6cf0ea0e'), '/WaltonDashboard/Motors/Hatch Power'],
+		[document.getElementById('w-node-2dd40fb21b0e-6cf0ea0e'), '/WaltonDashboard/Motors/Hatch Target'],
+		[document.getElementById('w-node-2d309eb47a59-6cf0ea0e'), '/WaltonDashboard/Motors/Cargo Power'],
+		[document.getElementById('w-node-4e1151804e61-6cf0ea0e'), '/WaltonDashboard/Motors/Cargo Target'],
+		[document.getElementById('w-node-1a75b8348af0-6cf0ea0e'), '/WaltonDashboard/Motors/Climber Power'],
+		[document.getElementById('w-node-e58751785c7a-6cf0ea0e'), '/WaltonDashboard/Motors/Climber Mode'],
+		[document.getElementById('w-node-828d4271a53a-6cf0ea0e'), '/WaltonDashboard/Drivetrain/Position'],
+		[document.getElementById('w-node-9971a8c5e074-6cf0ea0e'), '/WaltonDashboard/Drivetrain/Encoder Right'],
+		[document.getElementById('w-node-04c10c4157ee-6cf0ea0e'), '/WaltonDashboard/Drivetrain/Encoder Left'],
+		[document.getElementById('w-node-39646deb58ba-6cf0ea0e'), '/WaltonDashboard/Drivetrain/leftSpeed'],
+		[document.getElementById('w-node-6a6121c16e8e-6cf0ea0e'), '/WaltonDashboard/Drivetrain/rightSpeed'],
+		[document.getElementById('w-node-75d3c40bd623-6cf0ea0e'), '/WaltonDashboard/Drivetrain/leftMotor']
 	],
 	generalStatistics: [
-		[document.querySelector('.progress-ring-bv'), 'Diagnostics/Battery Voltage', 0.0, 12.0, 'v'],
-		[document.querySelector('.progress-ring-cpu'), 'Diagnostics/RIO CPU Use', 0.0, 100.0, '%'],
-		[document.querySelector('.progress-ring-pd'), 'Diagnostics/Power Draw', 0.0, 40.0, 'A'],
-		[document.querySelector('.progress-ring-ram'), 'Diagnostics/RIO RAM Use', 0.0, 100.0, '%']
+		[document.querySelector('.progress-ring-bv'), '/WaltonDashboard/Diagnostics/Battery Voltage', 0.0, 12.0, 'v'],
+		[document.querySelector('.progress-ring-cpu'), '/WaltonDashboard/Diagnostics/RIO CPU Use', 0.0, 100.0, '%'],
+		[document.querySelector('.progress-ring-pd'), '/WaltonDashboard/Diagnostics/Power Draw', 0.0, 40.0, 'A'],
+		[document.querySelector('.progress-ring-ram'), '/WaltonDashboard/Diagnostics/RIO RAM Use', 0.0, 100.0, '%']
 	],
 	powerUsageBox: [
-		[document.getElementById('fl-motor-pu'), 'Diagnostics/Front Left Motor Power Usage', 0.0, 25.0],
-		[document.getElementById('fr-motor-pu'), 'Diagnostics/Front Right Motor Power Usage', 0.0, 25.0],
-		[document.getElementById('bl-motor-pu'), 'Diagnostics/Back Left Motor Power Usage', 0.0, 25.0],
-		[document.getElementById('br-motor-pu'), 'Diagnostics/Back Right Motor Power Usage', 0.0, 25.0],
-		[document.getElementById('climber-motor-pu'), 'Diagnostics/Climber Motor Power Usage', 0.0, 25.0],
-		[document.getElementById('roborio-pu'), 'Diagnostics/RIO Power Usage', 0.0, 25.0]
+		[document.getElementById('fl-motor-pu'), '/WaltonDashboard/Diagnostics/Front Left Motor Power Usage', 0.0, 25.0],
+		[document.getElementById('fr-motor-pu'), '/WaltonDashboard/Diagnostics/Front Right Motor Power Usage', 0.0, 25.0],
+		[document.getElementById('bl-motor-pu'), '/WaltonDashboard/Diagnostics/Back Left Motor Power Usage', 0.0, 25.0],
+		[document.getElementById('br-motor-pu'), '/WaltonDashboard/Diagnostics/Back Right Motor Power Usage', 0.0, 25.0],
+		[document.getElementById('climber-motor-pu'), '/WaltonDashboard/Diagnostics/Climber Motor Power Usage', 0.0, 25.0],
+		[document.getElementById('roborio-pu'), '/WaltonDashboard/Diagnostics/RIO Power Usage', 0.0, 25.0]
 	],
 	motorPowersBox: [
-		[document.getElementById('fl-wheel-power'), 'Drivetrain/leftMotor', -1.0, 1.0],
-		[document.getElementById('fr-wheel-power'), 'Drivetrain/rightMotor', -1.0, 1.0],
-		[document.getElementById('bl-wheel-power'), 'Drivetrain/leftMotor', -1.0, 1.0],
-		[document.getElementById('br-wheel-power'), 'Drivetrain/rightMotor', -1.0, 1.0],
-		[document.getElementById('climber-motor-power'), 'Motors/Climber Power', -1.0, 1.0]
+		[document.getElementById('fl-wheel-power'), '/WaltonDashboard/Drivetrain/leftMotor', -1.0, 1.0],
+		[document.getElementById('fr-wheel-power'), '/WaltonDashboard/Drivetrain/rightMotor', -1.0, 1.0],
+		[document.getElementById('bl-wheel-power'), '/WaltonDashboard/Drivetrain/leftMotor', -1.0, 1.0],
+		[document.getElementById('br-wheel-power'), '/WaltonDashboard/Drivetrain/rightMotor', -1.0, 1.0],
+		[document.getElementById('climber-motor-power'), '/WaltonDashboard/Motors/Climber Power', -1.0, 1.0]
 	],
 	gyroscope: [
-		[document.getElementById('gyro'), 'Diagnostics/Gyro Orientation', 0.0]
+		[document.getElementById('gyro'), '/WaltonDashboard/Diagnostics/Gyro Orientation', 0.0]
 	]
 };
 
@@ -109,8 +109,9 @@ function onNetworkTablesChange(key, value, isNew) {
 	keyValuePair = ui.generalStatistics.find(kvp => kvp[1] === key);
 	
 	if (keyValuePair) {
-		var statisticCircle = keyValuePair[0].children[1].children[1];
-		var statisticText = keyValuePair[0].children[1].children[2];
+		alert('Hello');
+		var statisticCircle = keyValuePair[0].children[1].children[0];
+		var statisticText = keyValuePair[0].children[1].children[1];
 		
 		var rangeMin = keyValuePair[2];
 		var rangeMax = keyValuePair[3];
